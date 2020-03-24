084451cd8ca9a6e6615477bf09c4618fa022db30
Dave Nolan <dpn.nolan@gmail.com>
2020-03-24 16:08:26 -0400

    Updates

---
+++ precache-manifest.f018b9e4f961c24fcf1545092f069364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e78b29c17a1f7d1e8585c8ea78f2585",
+    "url": "/card-api/index.html"
+  },
+  {
+    "revision": "ed28d34442c99e39bf8e",
+    "url": "/card-api/static/css/main.ac2a942d.chunk.css"
+  },
+  {
+    "revision": "3a5ca467fcb1574ae7f4",
+    "url": "/card-api/static/js/2.5ef1ff58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-api/static/js/2.5ef1ff58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed28d34442c99e39bf8e",
+    "url": "/card-api/static/js/main.b83df616.chunk.js"
+  },
+  {
+    "revision": "e613fe43774dee8723fe",
+    "url": "/card-api/static/js/runtime-main.24fc396b.js"
+  }
+]);
